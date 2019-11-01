25ecebd37a3222b967858c21b25fe9b906542dd7
curry_glb <wyy_curry@163.com>
2019-11-01 20:13:04 +0800

    update package name

--- src/main/java/life/curry/communtiy/CommuntiyApplication.java
+++ src/main/java/life/curry/communtiy/CommuntiyApplication.java
@@ -1,4 +1,4 @@
-package com.life.communtiy;
+package life.curry.communtiy;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
