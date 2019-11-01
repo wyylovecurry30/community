41510f2f0e422adacd2320fc7e58d00d13d8573e
curry_glb <wyy_curry@163.com>
2019-11-01 18:27:48 +0800

    init re

---
+++ src/test/java/com/life/communtiy/CommuntiyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.life.communtiy;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommuntiyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
